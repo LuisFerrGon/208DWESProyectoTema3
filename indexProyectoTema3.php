46d19f3597e6cfe8677e1f6bcf7e4090b8b4e6d9
Luis Ferreras <luis.fergon.2@educa.jcyl.es>
2024-09-26 12:26:04 +0200

    Prueba

---
+++ indexProyectoTema3.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--Autor: Luis Ferreras González-->
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Index</title>
+        <link type="text/css" rel="stylesheet" href="webroot/estilos.css">
+    </head>
+    <body>
+        <header>
+            <h1>Desarrollo Web en Entorno Servidor</h1>
+        </header>
+        <nav>
+            <div>
+                <h2>Estudios</h2>
+                
+            </div>
+        </nav>
+        <footer>
+            <a href="https://github.com/LuisFerrGon/208DWESProyectoDWES">GitHub</a>
+            <a href="../doc/Curriculum.pdf">Curriculum</a>
+            <a href="../index.html">Página anterior</a>
+        </footer>
+    </body>
+</html>
