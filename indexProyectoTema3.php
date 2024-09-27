af297beddf2107fac93da0dbb6a15da6a323415f
Luis Ferreras <luis.fergon.2@educa.jcyl.es>
2024-09-27 08:43:43 +0200

    V1.1.1

--- indexProyectoTema3.php
+++ indexProyectoTema3.php
@@ -20,19 +20,19 @@
                     <td>0</td>
                     <td>Hola mundo y phpinfo()</td>
                     <td><a href="codigoPHP/ejercicio00.php">Ejecutar</a></td>
-                    <td><a href="mostrarcodigo/muestraEjercicio00">Mostrar</a></td>
+                    <td><a href="mostrarcodigo/muestraEjercicio00.php">Mostrar</a></td>
                 </tr><!--00-->
                 <tr>
                     <td>1</td>
                     <td>Variables</td>
                     <td><a href="codigoPHP/ejercicio01.php">Ejecutar</a></td>
-                    <td><a href="mostrarcodigo/muestraEjercicio01">Mostrar</a></td>
+                    <td><a href="mostrarcodigo/muestraEjercicio01.php">Mostrar</a></td>
                 </tr><!--01-->
                 <tr>
                     <td>2</td>
                     <td>Variables</td>
                     <td><a href="codigoPHP/ejercicio02.php">Ejecutar</a></td>
-                    <td><a href="mostrarcodigo/muestraEjercicio02">Mostrar</a></td>
+                    <td><a href="mostrarcodigo/muestraEjercicio02.php">Mostrar</a></td>
                 </tr><!--02-->
             </table>
         </main>
